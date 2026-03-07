2450b80d5f14406a60a1fff6b628f822ab4d3cea
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2026-03-07 06:33:50 +0000

    Add Axion Release plugin
    This does what I was trying to do before, just a lot better. Combines
    the ideas behind my old git versioner plugin and the better way of
    handling snapshots and releases I was going for here.
    
    Super neat.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,13 @@ plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.kotlin.serialization) apply false
   alias(libs.plugins.kotlinx.kover) apply false
+  alias(libs.plugins.axion.release)
   alias(libs.plugins.sonarqube)
   alias(libs.plugins.spotless)
 }
 
 group = "uk.co.developmentanddinosaurs.diplodokode"
-
-val releaseType = project.findProperty("releaseType") as String?
-val releaseVersion = project.findProperty("releaseVersion") as String?
-
-version =
-    when (releaseType) {
-      "release" -> releaseVersion ?: "0.0.1"
-      "snapshot" -> "${releaseVersion ?: "0.0.1"}-SNAPSHOT"
-      else -> "0.0.1-SNAPSHOT"
-    }
+version = scmVersion.version
 
 subprojects {
   group = rootProject.group
